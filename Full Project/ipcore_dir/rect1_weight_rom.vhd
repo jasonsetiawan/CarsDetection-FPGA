4995653ec73ed3553a2d7316e899d5b870ddfc25
Jason Danny Setiawan <46361419+jasonsetiawan@users.noreply.github.com>
2020-02-23 09:39:54 +0700

    Add files via upload

---
+++ Full Project/ipcore_dir/rect1_weight_rom.vhd
@@ -0,0 +1,108 @@
+--------------------------------------------------------------------------------
+--    This file is owned and controlled by Xilinx and must be used solely     --
+--    for design, simulation, implementation and creation of design files     --
+--    limited to Xilinx devices or technologies. Use with non-Xilinx          --
+--    devices or technologies is expressly prohibited and immediately         --
+--    terminates your license.                                                --
+--                                                                            --
+--    XILINX IS PROVIDING THIS DESIGN, CODE, OR INFORMATION "AS IS" SOLELY    --
+--    FOR USE IN DEVELOPING PROGRAMS AND SOLUTIONS FOR XILINX DEVICES.  BY    --
+--    PROVIDING THIS DESIGN, CODE, OR INFORMATION AS ONE POSSIBLE             --
+--    IMPLEMENTATION OF THIS FEATURE, APPLICATION OR STANDARD, XILINX IS      --
+--    MAKING NO REPRESENTATION THAT THIS IMPLEMENTATION IS FREE FROM ANY      --
+--    CLAIMS OF INFRINGEMENT, AND YOU ARE RESPONSIBLE FOR OBTAINING ANY       --
+--    RIGHTS YOU MAY REQUIRE FOR YOUR IMPLEMENTATION.  XILINX EXPRESSLY       --
+--    DISCLAIMS ANY WARRANTY WHATSOEVER WITH RESPECT TO THE ADEQUACY OF THE   --
+--    IMPLEMENTATION, INCLUDING BUT NOT LIMITED TO ANY WARRANTIES OR          --
+--    REPRESENTATIONS THAT THIS IMPLEMENTATION IS FREE FROM CLAIMS OF         --
+--    INFRINGEMENT, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A   --
+--    PARTICULAR PURPOSE.                                                     --
+--                                                                            --
+--    Xilinx products are not intended for use in life support appliances,    --
+--    devices, or systems.  Use in such applications are expressly            --
+--    prohibited.                                                             --
+--                                                                            --
+--    (c) Copyright 1995-2019 Xilinx, Inc.                                    --
+--    All rights reserved.                                                    --
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+-- You must compile the wrapper file rect1_weight_rom.vhd when simulating
+-- the core, rect1_weight_rom. When compiling the wrapper file, be sure to
+-- reference the XilinxCoreLib VHDL simulation library. For detailed
+-- instructions, please refer to the "CORE Generator Help".
+
+-- The synthesis directives "translate_off/translate_on" specified
+-- below are supported by Xilinx, Mentor Graphics and Synplicity
+-- synthesis tools. Ensure they are correct for your synthesis tool(s).
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+-- synthesis translate_off
+LIBRARY XilinxCoreLib;
+-- synthesis translate_on
+ENTITY rect1_weight_rom IS
+  PORT (
+    a : IN STD_LOGIC_VECTOR(6 DOWNTO 0);
+    clk : IN STD_LOGIC;
+    spo : OUT STD_LOGIC_VECTOR(21 DOWNTO 0)
+  );
+END rect1_weight_rom;
+
+ARCHITECTURE rect1_weight_rom_a OF rect1_weight_rom IS
+-- synthesis translate_off
+COMPONENT wrapped_rect1_weight_rom
+  PORT (
+    a : IN STD_LOGIC_VECTOR(6 DOWNTO 0);
+    clk : IN STD_LOGIC;
+    spo : OUT STD_LOGIC_VECTOR(21 DOWNTO 0)
+  );
+END COMPONENT;
+
+-- Configuration specification
+  FOR ALL : wrapped_rect1_weight_rom USE ENTITY XilinxCoreLib.dist_mem_gen_v7_2(behavioral)
+    GENERIC MAP (
+      c_addr_width => 7,
+      c_default_data => "0",
+      c_depth => 128,
+      c_family => "spartan3",
+      c_has_clk => 1,
+      c_has_d => 0,
+      c_has_dpo => 0,
+      c_has_dpra => 0,
+      c_has_i_ce => 0,
+      c_has_qdpo => 0,
+      c_has_qdpo_ce => 0,
+      c_has_qdpo_clk => 0,
+      c_has_qdpo_rst => 0,
+      c_has_qdpo_srst => 0,
+      c_has_qspo => 0,
+      c_has_qspo_ce => 0,
+      c_has_qspo_rst => 0,
+      c_has_qspo_srst => 0,
+      c_has_spo => 1,
+      c_has_spra => 0,
+      c_has_we => 0,
+      c_mem_init_file => "rect1_weight_rom.mif",
+      c_mem_type => 0,
+      c_parser_type => 1,
+      c_pipeline_stages => 0,
+      c_qce_joined => 0,
+      c_qualify_we => 0,
+      c_read_mif => 1,
+      c_reg_a_d_inputs => 1,
+      c_reg_dpra_input => 0,
+      c_sync_enable => 1,
+      c_width => 22
+    );
+-- synthesis translate_on
+BEGIN
+-- synthesis translate_off
+U0 : wrapped_rect1_weight_rom
+  PORT MAP (
+    a => a,
+    clk => clk,
+    spo => spo
+  );
+-- synthesis translate_on
+
+END rect1_weight_rom_a;
